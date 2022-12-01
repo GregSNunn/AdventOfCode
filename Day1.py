aafcf9ed898f205b103a92f3ff28b13743b68dfb
Greg Nunn <gregory.nunn@vermed.co.uk>
2022-12-01 16:27:49 +0000

    Comments

--- Day1.py
+++ Day1.py
@@ -1,11 +1,18 @@
+##############
+#Part 1
+##############
+
+#Bring in Data
 with open('Day1Input.txt') as f: 
     inputf=f.readlines()
 
 records=len(inputf)
 
+#Initial Variables
 maxcal = 0;
 elfcal = 0;
 
+#Loop to Determine Max Calories
 for i in inputf:
     
     if i != '\n':
@@ -16,4 +23,9 @@ for i in inputf:
       elfcal = 0
 
 print(maxcal)
+
+##############
+#Part 2
+##############
+
 
