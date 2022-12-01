294a8f9c3e790352e83f99d1917c0e5698417090
Greg Nunn <gregory.nunn@vermed.co.uk>
2022-12-01 16:22:13 +0000

    Final Day1 Version

--- Day1.py
+++ Day1.py
@@ -1,2 +1,19 @@
- with open('Day1Input.txt') as f: 
-    lines=f.readlines()
+with open('Day1Input.txt') as f: 
+    inputf=f.readlines()
+
+records=len(inputf)
+
+maxcal = 0;
+elfcal = 0;
+
+for i in inputf:
+    
+    if i != '\n':
+        elfcal= elfcal + int(i)
+    else: 
+      if maxcal < elfcal:
+        maxcal = elfcal
+      elfcal = 0
+
+print(maxcal)
+
