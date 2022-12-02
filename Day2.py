f679f0154636102fd49a80d1510fc23491059f17
Greg Nunn <gregory.nunn@vermed.co.uk>
2022-12-02 17:28:33 +0000

    Part 2 Final

--- Day2.py
+++ Day2.py
@@ -45,3 +45,41 @@ for j in games:
         scores.append(0)
         
 print(sum(scores))
+
+
+####################
+# Part 2
+#--------------------
+# X = Lose (0 point)
+# Y = Draw (3 points)
+# Z = Win  (6 points)
+####################
+
+scores2 = []
+
+for j in games:
+    if j[0] == 'A':
+        if j[1] == 'X': 
+            scores2.append(3)
+        elif j[1] == 'Y':
+            scores2.append(4)
+        elif j[1] == 'Z':
+            scores2.append(8)
+    elif j[0] == 'B':
+        if j[1] == 'X': 
+            scores2.append(1)
+        elif j[1] == 'Y':
+            scores2.append(5)
+        elif j[1] == 'Z':
+            scores2.append(9)
+    elif j[0] == 'C':
+        if j[1] == 'X': 
+            scores2.append(2)
+        elif j[1] == 'Y':
+            scores2.append(6)
+        elif j[1] == 'Z':
+            scores2.append(7)
+    else:
+        scores2.append(0)
+
+print(sum(scores2))
