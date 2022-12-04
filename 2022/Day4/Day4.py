325fde5b061d7ca9857ea04aef0616098c8514de
Greg Nunn <gregory.nunn@vermed.co.uk>
2022-12-04 16:50:39 +0000

    Final

--- 2022/Day4/Day4.py
+++ 2022/Day4/Day4.py
@@ -16,11 +16,29 @@ for i in inputf:
 #Determine if one includes the other
 incs = []
 for x in pairs:
-    inc_chk = False
-    if x[0][0] <= x[1][0] and x[0][1] >= x[1][1]:
+    if (int(x[0][0]) <= int(x[1][0])) and (int(x[0][1]) >= int(x[1][1])):
         inc_chk = True
-    elif x[1][0] <= x[0][0] and x[1][1] >= x[0][1]:
+    elif (int(x[1][0]) <= int(x[0][0])) and (int(x[1][1]) >= int(x[0][1])):
         inc_chk = True
+    else: 
+        inc_chk=False
     incs.append(inc_chk)
 
-print(incs)
+print(sum(incs))
+
+#Determine if they don't overlap to determine those that do
+ovrlp = []
+for x in pairs:
+    if (int(x[0][0]) < int(x[1][0])) and (int(x[0][1]) < int(x[1][0])):
+        ovrlp_chk = False
+    elif (int(x[0][0]) > int(x[1][1])) and (int(x[0][1]) < int(x[1][1])):
+        ovrlp_chk = False
+    elif (int(x[1][0]) < int(x[0][0])) and (int(x[1][1]) < int(x[0][0])):
+        ovrlp_chk = False
+    elif (int(x[1][0]) > int(x[0][1])) and (int(x[1][1]) > int(x[0][1])):
+        ovrlp_chk = False
+    else: 
+        ovrlp_chk=True
+    ovrlp.append(ovrlp_chk)  
+
+print(sum(ovrlp))
