dca10f0a1cc420d7ad09d47b41f10170256c3348
Greg Nunn <gregory.nunn@vermed.co.uk>
2022-12-08 17:31:09 +0000

    Day 8

--- 2022/Day8/Day8.py
+++ 2022/Day8/Day8.py
@@ -8,15 +8,11 @@ forest_init=[]
 for i in inputf:
     forest_init.append([*i.strip()])
 
-visible=[]
 forest=[]
 for i in forest_init:
-    line=[]
     fline=[]
     for j in i:
         fline.append(int(j))
-        line.append(False)
-    visible.append(line)
     forest.append(fline)
 
 def tree_chk(x,y):
@@ -29,10 +25,22 @@ def tree_chk(x,y):
         if forest[x][jx] > max3: max3=forest[x][jx]
     for jy in range(y+1, len(forest[i])):
         if forest[x][jy] > max4: max4=forest[x][jy]
-    if forest[x][y] > min(max1,max2,max3,max4): visible[x][y] = True
+    if forest[x][y] > min(max1,max2,max3,max4): return 1
+    else: return 0
+
+def tree_count(x,y,list):
+    count=0
+    if len(list) > 0:
+        for tree in list:
+            count = count+1
+            if tree >= forest[x][y]:
+                break
+    else:
+        count=1
+    return(count)
 
 def tree_sce(x,y):
-    up,left,right,down=[],[],.[],[]
+    up,left,right,down=[],[],[],[]
     for ix in range(0, x):
          left.append(forest[ix][y])
     for iy in range(x+1, len(forest)):
@@ -42,18 +50,28 @@ def tree_sce(x,y):
     for jy in range(y+1, len(forest[i])):
          down.append(forest[x][jy])
 
+    left.reverse()
+    up.reverse()
+
+    return(tree_count(x,y,left)*tree_count(x,y,up)*tree_count(x,y,right)*tree_count(x,y,down))
+   
+
 #Part 1
+tot=0
 for i in range(0, len(forest)):
     for j in range(0, len(forest[i])):
         if i == 0 or i == len(forest)-1:
-            visible[i][j] = True
+            tot = tot+1
         elif j==0 or j == len(forest[i])-1:
-            visible[i][j] = True
+            tot = tot+1
         else:
-            tree_chk(i, j)
-
-tot=0
-for line in visible:
-    #print(line)
-    tot = tot + sum(line)
+            tot = tot+tree_chk(i, j)
 print(tot)
+
+#Part 1
+ans=0
+for i in range(0, len(forest)):
+    for j in range(0, len(forest[i])):
+        ans = max(ans,tree_sce(i, j))
+
+print(ans)
